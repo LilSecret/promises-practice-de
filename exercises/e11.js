b60aa17c3c4a219351ec9f42349d414e08c2123b
Charles Marchant <chuckm248@gmail.com>
2023-09-11 20:47:30 -0700

    exercise 11

--- exercises/e11.js
+++ exercises/e11.js
@@ -14,6 +14,7 @@
 */
 
 export const usersUrl = 'http://localhost:3000/users/';
+import fetch from "node-fetch";
 
 /**
  * @task
@@ -26,9 +27,9 @@ export const usersUrl = 'http://localhost:3000/users/';
  * Example: const getLoginList = (data) => {<Your code>}
 */
 
-const getLoginList = () => {
+const getLoginList = (data) => {
   // Your code goes here...
-
+  return data.map((user => user.login));
 }
 
 /**
@@ -39,7 +40,7 @@ const getLoginList = () => {
 */
 
 // Your code goes here ...
-const getData;
+const getData = fetch(usersUrl);
 
 /**
  * @task 
@@ -53,7 +54,12 @@ const getData;
 */
 
 // Your code goes here ...
-export const result = getData;
+export const result = getData
+  .then(data => data.json())
+  .then(data => getLoginList(data))
+  .then(array => console.log(array))
+  .catch(err => err);
+  // .then(array => array)
 
 
 // === TEST YOURSELF ===
